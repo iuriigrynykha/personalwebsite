e18f9ed0d4a37441f8cbf7d3d14539ebf912e3f7
Iurii <geroge.gru@gmail.com>
2016-10-12 12:56:52 +0000

    My personal webpage

--- js/index.js
+++ js/index.js
@@ -6,14 +6,12 @@ $(function() {
             $('.top-layout-positioner').addClass('affix-top');
             $('.header-logo').addClass('affix');
             $('.header-nav-menu ul li a').css('font-size', '0.8em');
-            $('.slicknav_menu').css('top', '-7px');
-            $('.slicknav_btn').css('margin-bottom', '4px');
+            $('.slicknav_btn').css('margin-bottom', '10px');
         } else { 
             $('.top-layout-positioner').removeClass('affix-top');
             $('.header-logo').removeClass('affix');
             $('.header-nav-menu ul li a').css('font-size', '1.2em');
-            $('.slicknav_menu').css('top', '0');
-            $('.slicknav_btn').css('margin-bottom', '11px');
+            $('.slicknav_btn').css('margin-bottom', '10px');
         }
     });
     $('#menu').slicknav();
