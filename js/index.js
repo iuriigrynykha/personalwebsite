460e60fe4366570c5e1fa7a41d2c316641a4c072
Iurii <geroge.gru@gmail.com>
2016-10-17 15:38:46 +0000

    Changes #2

--- js/index.js
+++ js/index.js
@@ -39,19 +39,24 @@ $(function() {
         $('form').trigger("reset");
     }).fail(function(xhr, err) {
         //$('#send').html(xhr.statusText);
-        $('#send').hide();
-        $('.warrning').show(function() {
-            $('#user-name-field, #user-email-field, #comments-field').on('focus', function() {
+        //$('#send').hide();
+        //$('.warrning').show(function() {
+           // $('#user-name-field, #user-email-field, #comments-field').on('focus', function() {
+              //  $('.warrning').hide();
+              //  $('#send').show();
+           // });
+       // });
+        $('#send').on('click', function() {
+            if ($.trim($("#user-name-field").val()) === "" || $.trim($("#user-email-field").val()) === "") {
+                $(this).hide();
+                $('.warrning').show(function() {
+           $('#user-name-field, #user-email-field, #comments-field').on('focus', function() {
                 $('.warrning').hide();
                 $('#send').show();
-            });
+           });
+        
         });
-        $('.contact-form').submit(function() {
-            if ($.trim($("#user-name-field").val()) === "" || $.trim($("#user-email-field").val()) === "") {
-                alert('you did not fill out one of the fields');
-                return false;
             }
-        });
         $('form,').trigger("reset");
     });
 });
